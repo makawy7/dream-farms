882b56f603b90975e56308b11b7b10fd36381a1f
abdallah <abdallahelmekawy6@gmail.com>
2023-03-16 22:49:35 +0200

    settings and users table

--- database/migrations/2023_03_16_200439_create_settings_table.php
+++ database/migrations/2023_03_16_200439_create_settings_table.php
@@ -13,6 +13,28 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string("website_name_en");
+            $table->string("website_name_ar");
+            $table->string("default_locale", 2);
+            $table->string("email")->nullable();
+            $table->string("currency", 3);
+            $table->string("logo_url");
+            $table->string("motto_en")->nullable();
+            $table->string("motto_ar")->nullable();
+            $table->text("info_en")->nullable();
+            $table->text("info_ar")->nullable();
+            $table->string("phone_numbers")->nullable();
+            $table->text("address_en")->nullable()->nullable();
+            $table->text("address_ar")->nullable();
+            $table->text("working_days_en")->nullable();
+            $table->text("working_days_ar")->nullable();
+            $table->string("map_location")->nullable();
+            $table->text("contact_head_en")->nullable();
+            $table->text("contact_head_ar")->nullable();
+            $table->string("copyright_info")->nullable();
+            $table->string("social_facebook")->nullable();
+            $table->string("social_twitter")->nullable();
+            $table->string("social_instagram")->nullable();
             $table->timestamps();
         });
     }
