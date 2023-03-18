f6e9e397c742aa17ffd43a1bca72bb5b5aa274af
abdallah <abdallahelmekawy6@gmail.com>
2023-03-18 22:19:25 +0200

    user preferred locale

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('locale')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->boolean('active')->default(true);
             $table->rememberToken();
