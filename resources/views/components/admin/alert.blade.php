9ac4b788c893e67f381b7868319168a99bb8fe14
abdallah <abdallahelmekawy6@gmail.com>
2023-03-25 05:42:13 +0200

    fix alert message style

--- resources/views/components/admin/alert.blade.php
+++ resources/views/components/admin/alert.blade.php
@@ -1,6 +1,6 @@
 @if (session('success'))
     <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => { show = false; }, 3000)"
-        class="alert alert-error alert-dismissible fade show" role="alert">
+        class="alert alert-success alert-dismissible fade show" role="alert">
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close"></button>
     </div>
