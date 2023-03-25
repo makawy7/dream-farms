6d16cf7fbef58b4cf9f80a0a0456e1cd1c5f1aeb
abdallah <abdallahelmekawy6@gmail.com>
2023-03-25 05:39:00 +0200

    delete old logo if new one is uploaded

---
+++ app/Observers/SettingObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Setting;
+use Illuminate\Support\Facades\Storage;
+
+class SettingObserver
+{
+    /**
+     * Handle the Setting "created" event.
+     */
+    public function created(Setting $setting): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Setting "updated" event.
+     */
+    public function updated(Setting $setting): void
+    {
+        if ($setting->isDirty('logo_url') && $oldLogo = $setting->getOriginal('logo_url')) {
+            Storage::disk('public')->delete($oldLogo);
+        }
+    }
+
+    /**
+     * Handle the Setting "deleted" event.
+     */
+    public function deleted(Setting $setting): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Setting "restored" event.
+     */
+    public function restored(Setting $setting): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Setting "force deleted" event.
+     */
+    public function forceDeleted(Setting $setting): void
+    {
+        //
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use App\Observers\SettingObserver;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,9 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
+    protected $observers = [
+        Setting::class => SettingObserver::class,
+    ];
     /**
      * Register any events for your application.
      */
