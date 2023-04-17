267f0e6f2d3b0fd29f729fc7c9762a56dd97fa0b
abdallah <abdallahelmekawy6@gmail.com>
2023-04-17 23:49:44 +0200

    product category request

---
+++ app/Http/Requests/Product/ProductCategoryRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name_en' => 'required|string|min:2|max:255',
+            'name_ar' => 'required|string|min:2|max:255',
+        ];
+    }
+}
