66d39d68e9907990445e2e92ea803162f2151e69
abdallah <abdallahelmekawy6@gmail.com>
2023-04-19 23:43:42 +0200

    generate unique slugs for the products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,26 @@ class Product extends Model
     use HasFactory;
     protected $guarded = [];
 
+    public function setSlugArAttribute($slug_ar)
+    {
+        $this->attributes['slug_ar'] = self::uniqueSlug($slug_ar);
+    }
+    public function setSlugEnAttribute($slug_en)
+    {
+        $this->attributes['slug_en'] = self::uniqueSlug($slug_en);
+    }
+
+    private static function uniqueSlug($slug, $counter = 1)
+    {
+        $newSlug = parent::where('slug', $slug)->exists() ? $slug . '-' . $counter : $slug;
+        if (parent::where('slug', $newSlug)->exists()) {
+            // if it still exists regenerate
+            $counter += 1;
+            return self::uniqueSlug($slug, $counter);
+        } else {
+            return $newSlug;
+        }
+    }
     public function category()
     {
         return $this->belongsTo(ProductCategory::class, 'category_id');
